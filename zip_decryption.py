faf1e2be684c2a566ee3cbe700b83ebfc652f0d5
Haedong-Jeon <haedongjeon@icloud.com>
2020-11-23 10:04:11 +0900

    파이선 zip 크래커 업데이트

--- zip_decryption.py
+++ zip_decryption.py
@@ -10,16 +10,19 @@ def crackzip(zfile, passwd):
     return False
 
 def main():
+    tryCount = 0
     dictionary_file = 'dictionary.txt'
     zip_file = 'locked.zip'
     zfile = zipfile.ZipFile(zip_file, 'r')
     pfile = open(dictionary_file, 'r')
 
-    print("*** DECRYPTION START  ***")
+    print("***DECRYPTION START ***")
     for line in pfile.readlines():
+        tryCount += 1
         passwd = line.strip('\n')
         t = Thread(target=crackzip, args = (zfile, passwd))
         t.start()
+        print('** NOW DECRYPTING... %d try' %(tryCount))
     print("** FINISHED **")
 
 main()
